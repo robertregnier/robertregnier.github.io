375eb5971f56c412e8b5161147cadfb00072c63b
Robert Regnier <robert_regnier@hotmail.com>
2025-01-12 10:04:35 -0600

    Analytics

---
+++ gtag.js
@@ -0,0 +1,9 @@
+﻿<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MMV90RE0VW"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-MMV90RE0VW');
+</script>
