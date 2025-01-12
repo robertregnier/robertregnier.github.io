7f56569be19cbf442ac67a5d18d905f95c897309
Robert Regnier <robert_regnier@hotmail.com>
2025-01-12 11:20:05 -0600

    Updated GA tag

--- gtag.js
+++
@@ -1,9 +0,0 @@
-﻿<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C991MNST01"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-C991MNST01');
-</script>
